e093243f573c5ddc3f5cc0bf6ddb759f7e095954
SushmitaShastri <54973671+SushmitaShastri@users.noreply.github.com>
2023-09-22 22:34:09 +0530

    Delete raconInteriorConstructions directory

--- raconInteriorConstructions/js/script.js
+++
@@ -1,7 +0,0 @@
-// Initialize the Bootstrap carousel
-$('.carousel').carousel();
-
-// Automatically play the video when it's focused
-$('#videoModal').on('shown.bs.modal', function () {
-    $('#video').get(0).play();
-});
